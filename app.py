ce6bc7e286e01f7683638ae75111efed39486c49
Alisson Menezes <alisson.copyleft@gmail.com>
2017-05-25 18:39:55 -0300

    hello world do openshift

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello estou rodando dentro do Openshift Origin"
+  
+ if __name__ == '__main__':
+    app.run(debug=True,threaded=True) 
