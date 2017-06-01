fac2bc7b7218648077081f8d1960fcce3506605a
Alisson Menezes <alisson.copyleft@gmail.com>
2017-06-01 11:56:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return "Hello estou rodando dentro do Openshift Origin"
 
 if __name__ == '__main__':
-    app.run(debug=True,threaded=True) 
+    app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
