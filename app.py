b2893ccda549288eb7d48b07aa11887a3dd878eb
alisson machado <alisson.machado@4linux.com.br>
2017-06-14 16:29:41 -0300

    deploy 2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello estou rodando dentro do Openshift Origin (Workshop 2)"
+    return "Hello estou rodando dentro do Openshift Origin (Workshop 2 deploy 2)"
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
