6c16477a85f7b963fd0651db82f3e9166cb4b92d
alisson machado <alisson.machado@4linux.com.br>
2017-07-21 14:40:46 -0300

    deploy 2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Deploy live no TDC"
+    return "Deploy live no TDC versao2"
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
