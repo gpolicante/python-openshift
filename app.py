b70f696588549ed2a27bd40af9bedb1531d0495a
Alisson Menezes <alisson.copyleft@gmail.com>
2017-12-20 20:55:11 -0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Deploy Algar"
+    return "Deploy Review03"
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
