125a4b7df95cee39ae03e897c46da5c78df764f7
Alisson Machado <alisson.copyleft@gmail.com>
2018-02-18 11:24:57 -0300

    comitando pro minishift

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Deploy Review03"
+    return "Fazendo teste no minishift"
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
