285eeb17bd1a64c26743ba9396b94d9026e75fa2
Alisson Machado <alisson.copyleft@gmail.com>
2018-03-11 20:51:38 -0300

    aula de volumes

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
-    return "Fazendo teste no minishift"
+    if (request.method == "POST"):
+        foto = request.files["foto"]
+        foto.save(os.path.abspath(os.path.dirname(__file__))+"/static/imagens/%s"%foto.filename)
+    fotos = os.listdir("static/imagens/")
+    return render_template("index.html",fotos=fotos)
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=True,host="0.0.0.0",port=8080) 
