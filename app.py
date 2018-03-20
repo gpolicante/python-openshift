80d7f668aaf9a622c9198d0e825e1e1a27c27065
Alisson Menezes <alisson.copyleft@gmail.com>
2018-03-19 21:00:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ def index():
         foto = request.files["foto"]
         foto.save(os.path.abspath(os.path.dirname(__file__))+"/static/imagens/%s"%foto.filename)
     fotos = os.listdir(os.path.abspath(os.path.dirname(__file__))+"/static/imagens/")
-    return render_template("index.html",fotos=fotos,usuario=usuario)
+    var1 = os.environ["var1"]
+    var2 = os.environ["var2"]
+    return render_template("index.html",fotos=fotos,usuario=usuario,var1=var1,var2=var2)
     
 @app.route("/login")
 def login():
