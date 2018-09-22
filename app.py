fdfe1a7e8120db31aac96e564dafebac11705669
gpolicante <38521838+gpolicante@users.noreply.github.com>
2018-09-21 21:04:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return "Fazendo teste no minishift"
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=8080) 
+    app.run(debug=True,host="127.0.0.1",port=8080) 
